91cb37ef88bac37e4f6bf0fe6d167a8354f771b2
TejasSai07 <147263041+TejasSai07@users.noreply.github.com>
2023-10-16 13:12:41 +0530

    Update LiverClass.java

--- OOP_Project/src/LiverClass.java
+++ OOP_Project/src/LiverClass.java
@@ -3,6 +3,11 @@ package Liver;
 public class Liver extends MedicalData {
   protected String BloodType; // Liver Blood Type
   protected String Hospital; // Hospital Name
+  public void Liver() //Default Constructor
+  {
+    BloodType=" ";
+    Hospital=" ";
+  }
   public void Liver(String B, String H)
   {
     super(B);
